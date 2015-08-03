d8fde7f3066259a047e45dd70e898fc19d4cc207
Luke <lharback@gmail.com>
2015-08-03 10:21:52 +1000

    Initial Commit

---
+++ Conway2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Conway2
+//
+//  Created by Luke Harback on 3/08/2015.
+//  Copyright © 2015 Luke Harback. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Conway2Tests/Conway2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Conway2Tests.swift
+//  Conway2Tests
+//
+//  Created by Luke Harback on 3/08/2015.
+//  Copyright © 2015 Luke Harback. All rights reserved.
+//
+
+import XCTest
+@testable import Conway2
+
+class Conway2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
